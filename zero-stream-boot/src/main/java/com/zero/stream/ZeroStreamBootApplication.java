70eed8b42038b682da5e0c65352dee14c5ad68a1
Micro-Organism <coolrabbit1993@gmail.com>
2024-10-25 11:00:22 +0800

    feat: springboot 集成 stream

---
+++ zero-stream-boot/src/main/java/com/zero/stream/ZeroStreamBootApplication.java
@@ -0,0 +1,13 @@
+package com.zero.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroStreamBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroStreamBootApplication.class, args);
+    }
+
+}
